38c52c1710cd08b635d89bfbbefa8a45ac2bef12
unknown <nateware777@gmail.com>
2024-10-26 18:36:15 +0300

    Building a Tower Of Stars

---
+++ BuildTower.js
@@ -0,0 +1,41 @@
+// Build Tower
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+
+// For example, a tower with 3 floors looks like this:
+
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+// And a tower with 6 floors looks like this:
+
+// [
+//   "     *     ", 
+//   "    ***    ", 
+//   "   *****   ", 
+//   "  *******  ", 
+//   " ********* ", 
+//   "***********"
+// ]
+
+
+
+
+
+
+
+
+
+
+//My Solution
+
+function towerBuilder(nFloors) {
+    let tower = []
+    for(let i = 0; i < nFloors; i++){
+    let stars = '*'.repeat(2 * i + 1)
+      let spaces = ' '.repeat(nFloors - i - 1)
+      tower.push(spaces + stars + spaces)
+  }  
+  return tower
+  }
